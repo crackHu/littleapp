0fdc5ce72342ca786374a6c4df1463e484b32602
crackHu-ba <296717367@qq.com>
2017-01-18 10:21:02 +0800

    auto push by batch

--- server/springboot_demo/src/main/java/tk/mybatis/mapper/mapper/CrawlerVerifyIpMapper.java
+++
@@ -1,7 +0,0 @@
-package tk.mybatis.mapper.mapper;
-
-import tk.mybatis.mapper.model.CrawlerVerifyIp;
-import tk.mybatis.springboot.util.MyMapper;
-
-public interface CrawlerVerifyIpMapper extends MyMapper<CrawlerVerifyIp> {
-}
--- server/springboot_demo/src/main/java/tk/mybatis/mapper/mapper/LoggingEventExceptionMapper.java
+++
@@ -1,7 +0,0 @@
-package tk.mybatis.mapper.mapper;
-
-import tk.mybatis.mapper.model.LoggingEventException;
-import tk.mybatis.springboot.util.MyMapper;
-
-public interface LoggingEventExceptionMapper extends MyMapper<LoggingEventException> {
-}

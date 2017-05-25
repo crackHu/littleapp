8159ac469622c7fc9b1805f3a4443def2dcaf921
crackHu <296717367@qq.com>
2017-05-26 04:15:54 +0800

    auto push by batch

--- server/wechat-littleapp-server/src/main/java/ml/littleapp/crawler/concurrent/impl/IpSrcPagingTask.java
+++ server/wechat-littleapp-server/src/main/java/ml/littleapp/crawler/concurrent/impl/IpSrcPagingTask.java
@@ -8,14 +8,25 @@ import ml.littleapp.crawler.concurrent.Crawler;
 
 public class IpSrcPagingTask extends RecursiveTask<Document> implements Crawler {
 	
+	private final static int THRESHOLD = 2;
+	
+	private String domain;
+	
 	private int pageTotal;
 	
-	public IpSrcPagingTask(int pageTotal) {
+	public IpSrcPagingTask(String domain, int pageTotal) {
+		this.domain = domain;
 		this.pageTotal = pageTotal;
 	}
 
 	@Override
 	protected Document compute() {
+		
+		int avg = pageTotal / 3;
+		int start = 0;
+		int end = pageTotal;
+				
+		boolean canCompute = (end - start) <= THRESHOLD;
 		
 		return crawler("");
 	}

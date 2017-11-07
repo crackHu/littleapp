b82993b02688470a220f20d4168baffb4971c364
crackHu <296717367@qq.com>
2017-11-08 00:36:43 +0800

    update

--- server/wechat-littleapp-server/src/main/java/ml/littleapp/util/concurrent/Test.java
+++ server/wechat-littleapp-server/src/main/java/ml/littleapp/util/concurrent/Test.java
@@ -40,6 +40,7 @@ public class Test {
 
 	}
 
+	// 1. 懒汉，线程安全, 效率低下
 	private static class LazyLoad {
 
 		public LazyLoad() {
@@ -58,6 +59,7 @@ public class Test {
 		}
 	}
 
+	// （双重校验锁）
 	private static class DoubleCheck {
 
 		private DoubleCheck() {
@@ -78,7 +80,8 @@ public class Test {
 			return doubleCheck;
 		}
 	}
-
+	
+	// Volatile （双重校验锁）
 	private static class VolatileInstance {
 		private VolatileInstance() {
 			System.out.println("VolatileInstance create");
@@ -99,6 +102,7 @@ public class Test {
 		}
 	}
 	
+	// 利用 classloader（静态内部类）
 	private static class LazyInnerClass {
 		private LazyInnerClass() {
 			System.out.println("LazyInnerClass create");
@@ -107,6 +111,9 @@ public class Test {
 		private static class SingletonHolder {
 			public SingletonHolder() {
 				System.out.println(4444);
+			}
+			static {
+				System.out.println(4445);
 			}
 			public static LazyInnerClass instance = new LazyInnerClass();
 		}
@@ -117,68 +124,72 @@ public class Test {
 	}
 
 	public static void main(String[] args) {
+		
+		LazyInnerClass.getInstance();
+		LazyInnerClass.getInstance();
+		LazyInnerClass.getInstance();
 
 		// 100000 singleton: 6 | lazySingleton: 11
 		// 20000 * 5 singleton: 1 1 1 2 3 | lazySingleton: 9 11 15 15 16 | double: 4 5 4 5 6 | volatile: 5 6 4 4 4 | lazyclass: 5 6 5 4 4
 
-		Runnable singleton = () -> {
-			long begin = System.currentTimeMillis();
-			for (int i = 0; i < 20000; i++) {
-				Test instance = Test.getInstance();
-			}
-			System.err.println("singleton:" + (System.currentTimeMillis() - begin) + Thread.currentThread().getName());
-		};
-
-		Runnable lazySingleton = () -> {
-			long begin = System.currentTimeMillis();
-			for (int i = 0; i < 20000; i++) {
-				LazyLoad instance = LazyLoad.getInstance();
-			}
-			System.err.println(
-					"lazySingleton:" + (System.currentTimeMillis() - begin) + Thread.currentThread().getName());
-		};
-
-		Runnable doubleCheck = () -> {
-			long begin = System.currentTimeMillis();
-			for (int i = 0; i < 20000; i++) {
-				DoubleCheck instance = DoubleCheck.getInstance();
-			}
-			System.err
-					.println("doubleCheck:" + (System.currentTimeMillis() - begin) + Thread.currentThread().getName());
-		};
-
-		Runnable volatileInstance = () -> {
-			long begin = System.currentTimeMillis();
-			for (int i = 0; i < 20000; i++) {
-				VolatileInstance instance = VolatileInstance.getInstance();
-			}
-			System.err.println(
-					"VolatileInstance:" + (System.currentTimeMillis() - begin) + Thread.currentThread().getName());
-		};
-		
-		Runnable lazyInner = () -> {
-			long begin = System.currentTimeMillis();
-			for (int i = 0; i < 20000; i++) {
-				LazyInnerClass instance = LazyInnerClass.getInstance();
-			}
-			System.err.println(
-					"LazyInnerClass:" + (System.currentTimeMillis() - begin) + Thread.currentThread().getName());
-		};
-
-		ExecutorService service = Executors.newCachedThreadPool();
-		try {
-			for (int i = 0; i < 5; i++) {
-				Future<?> submit = service.submit(singleton);
-				service.submit(lazySingleton);
-				service.submit(doubleCheck);
-				service.submit(volatileInstance);
-				service.submit(lazyInner);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			service.shutdown();
-		}
+//		Runnable singleton = () -> {
+//			long begin = System.currentTimeMillis();
+//			for (int i = 0; i < 20000; i++) {
+//				Test instance = Test.getInstance();
+//			}
+//			System.err.println("singleton:" + (System.currentTimeMillis() - begin) + Thread.currentThread().getName());
+//		};
+//
+//		Runnable lazySingleton = () -> {
+//			long begin = System.currentTimeMillis();
+//			for (int i = 0; i < 20000; i++) {
+//				LazyLoad instance = LazyLoad.getInstance();
+//			}
+//			System.err.println(
+//					"lazySingleton:" + (System.currentTimeMillis() - begin) + Thread.currentThread().getName());
+//		};
+//
+//		Runnable doubleCheck = () -> {
+//			long begin = System.currentTimeMillis();
+//			for (int i = 0; i < 20000; i++) {
+//				DoubleCheck instance = DoubleCheck.getInstance();
+//			}
+//			System.err
+//					.println("doubleCheck:" + (System.currentTimeMillis() - begin) + Thread.currentThread().getName());
+//		};
+//
+//		Runnable volatileInstance = () -> {
+//			long begin = System.currentTimeMillis();
+//			for (int i = 0; i < 20000; i++) {
+//				VolatileInstance instance = VolatileInstance.getInstance();
+//			}
+//			System.err.println(
+//					"VolatileInstance:" + (System.currentTimeMillis() - begin) + Thread.currentThread().getName());
+//		};
+//		
+//		Runnable lazyInner = () -> {
+//			long begin = System.currentTimeMillis();
+//			for (int i = 0; i < 20000; i++) {
+//				LazyInnerClass instance = LazyInnerClass.getInstance();
+//			}
+//			System.err.println(
+//					"LazyInnerClass:" + (System.currentTimeMillis() - begin) + Thread.currentThread().getName());
+//		};
+//
+//		ExecutorService service = Executors.newCachedThreadPool();
+//		try {
+//			for (int i = 0; i < 5; i++) {
+//				Future<?> submit = service.submit(singleton);
+//				service.submit(lazySingleton);
+//				service.submit(doubleCheck);
+//				service.submit(volatileInstance);
+//				service.submit(lazyInner);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		} finally {
+//			service.shutdown();
+//		}
 
 	}
 }
